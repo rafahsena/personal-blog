96d5d54db9860cc592b989eba70bd3ecb47f47bd
Rafael Sena <rafaelsenna13@gmail.com>
2021-02-15 03:43:13 -0300

    added sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,14 +1,76 @@
-import { Box, Text, useStyleConfig } from "@chakra-ui/react";
 import React from "react";
+import {
+  Flex,
+  useStyleConfig,
+  VStack,
+  Link,
+  Input,
+  IconButton,
+  HStack,
+  Icon,
+} from "@chakra-ui/react";
+
+import { SearchIcon } from "@chakra-ui/icons";
+
+import { GrGithub, GrSpotify, GrLinkedin } from "react-icons/gr";
 
 const Sidebar = () => {
-  
-  const style = useStyleConfig("Sidebar");
+  const styles = useStyleConfig("Sidebar");
 
   return (
-    <Box padding={8} {...style}>
-      <Text>Sidebar</Text>
-    </Box>
+    <Flex {...styles}>
+      <VStack spacing={4}>
+        <Link
+          href="https://rafahsena.dev"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Página Inicial
+        </Link>
+        <Link
+          href="https://portfolio.rafahsena.dev"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Portfólio
+        </Link>
+        <HStack>
+          <Input
+            _placeholder={{ color: styles.color }}
+            variant="filled"
+            placeholder="Buscar por texto ou tag"
+          />
+          <IconButton
+            colorScheme="blue"
+            aria-label="Search articles"
+            icon={<SearchIcon />}
+          />
+        </HStack>
+        <HStack marginTop={4} spacing={4}>
+          <Link
+            href="https://github.com/rafahsena"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <Icon h={6} w={6} as={GrGithub} />
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/rafahsena/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <Icon h={6} w={6} as={GrLinkedin} />
+          </Link>
+          <Link
+            href="https://open.spotify.com/user/rafa_sena?si=BkKmFbkpTSSBktjVkdk66A"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <Icon h={6} w={6} as={GrSpotify} />
+          </Link>
+        </HStack>
+      </VStack>
+    </Flex>
   );
 };
 
--- src/theme/components/sidebar.js
+++ src/theme/components/sidebar.js
@@ -1,8 +1,11 @@
 const sidebarTheme = {
-    baseStyle: ({ colorMode }) => ({
-      bg: colorMode === "light" ? "#E2E8F0" : "#202225",
-    }),
-  };
-  
-  export default sidebarTheme;
-  
+  baseStyle: ({ colorMode }) => ({
+    bg: colorMode === "light" ? "#E2E8F0" : "#202225",
+    justifyContent: "center",
+    alignItems: "center",
+    fontFamily: "'Open Sans',sans-serif",
+    color: colorMode === "light" ? "rgba(0, 0, 0, 0.6)" : "rgba(255, 255, 255, 0.6)",
+  }),
+};
+
+export default sidebarTheme;
